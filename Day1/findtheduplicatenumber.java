70c1ff200ee1c2588061aaa29734a7b26e46108f
A V REVANTH <105591606+ayitharevanth@users.noreply.github.com>
2024-07-14 19:23:35 +0530

    Create findtheduplicatenumber.java

---
+++ Day1/findtheduplicatenumber.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer>hs = new HashSet<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!hs.contains(nums[i]))
+            {
+                hs.add(nums[i]);
+            }
+            else
+            {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
