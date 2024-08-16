3eeac20f521636cd34d9598b0599c63c61924c16
A V REVANTH <105591606+ayitharevanth@users.noreply.github.com>
2024-08-16 23:41:34 +0530

    Create Remove Duplicates from Sor.ted List II.java

---
+++ Week5/Remove Duplicates from Sor.ted List II.java
@@ -0,0 +1,21 @@
+public class Solution {
+public ListNode deleteDuplicates(ListNode head) {
+	//use two pointers, slow - track the node before the dup nodes, 
+	// fast - to find the last node of dups.
+    ListNode dummy = new ListNode(0), fast = head, slow = dummy;
+    slow.next = fast;
+    while(fast != null) {
+    	while (fast.next != null && fast.val == fast.next.val) {
+     		fast = fast.next;    //while loop to find the last node of the dups.
+    	}
+    	if (slow.next != fast) { //duplicates detected.
+    		slow.next = fast.next; //remove the dups.
+    		fast = slow.next;     //reposition the fast pointer.
+    	} else { //no dup, move down both pointer.
+    		slow = slow.next;
+    		fast = fast.next;
+    	}
+    	
+    }
+    return dummy.next;
+} }
