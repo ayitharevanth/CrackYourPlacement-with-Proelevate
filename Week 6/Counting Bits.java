3c80cbed518f1e165417fa87b327f3daf6372f22
A V REVANTH <105591606+ayitharevanth@users.noreply.github.com>
2024-08-21 04:54:10 +0530

    Create Counting Bits.java

---
+++ Week 6/Counting Bits.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[] countBits(int num) {
+        int[] result = new int[num + 1];
+        result[0] = 0;
+        for (int i = 1; i <= num; i++) {
+            result[i] = result[i&(i-1)] + 1;
+        }
+        return result;
+    }
+}
