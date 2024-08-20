21973924a091ab7b08d5c82ae3c43b5d91903545
A V REVANTH <105591606+ayitharevanth@users.noreply.github.com>
2024-08-20 05:43:07 +0530

    Create  Ugly Number II.java

---
+++ Week 6/Ugly Number II.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] uglyNumbers = new int[n]; // DP array to store ugly numbers
+        uglyNumbers[0] = 1; // The first ugly number is 1
+
+        // Three pointers for the multiples of 2, 3, and 5
+        int indexMultipleOf2 = 0, indexMultipleOf3 = 0, indexMultipleOf5 = 0;
+        int nextMultipleOf2 = 2, nextMultipleOf3 = 3, nextMultipleOf5 = 5;
+
+        // Generate ugly numbers until we reach the nth one
+        for (int i = 1; i < n; i++) {
+            // Find the next ugly number as the minimum of the next multiples
+            int nextUglyNumber = Math.min(
+                nextMultipleOf2,
+                Math.min(nextMultipleOf3, nextMultipleOf5)
+            );
+            uglyNumbers[i] = nextUglyNumber;
+
+            // Update the corresponding pointer and next multiple
+            if (nextUglyNumber == nextMultipleOf2) {
+                indexMultipleOf2++;
+                nextMultipleOf2 = uglyNumbers[indexMultipleOf2] * 2;
+            }
+            if (nextUglyNumber == nextMultipleOf3) {
+                indexMultipleOf3++;
+                nextMultipleOf3 = uglyNumbers[indexMultipleOf3] * 3;
+            }
+            if (nextUglyNumber == nextMultipleOf5) {
+                indexMultipleOf5++;
+                nextMultipleOf5 = uglyNumbers[indexMultipleOf5] * 5;
+            }
+        }
+
+        return uglyNumbers[n - 1]; // Return the nth ugly number
+    }
+}
